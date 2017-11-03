7f51b65b8910592122041a91b7cca24115fcc03d
Lakerolmaker <jacob.n.olsson@hotmail.com>
2017-11-03 15:50:00 +0100

    Added the baseline
    Added the library and some of the files that we needed.

---
+++ src/FileClass.java
@@ -0,0 +1,142 @@
+import java.awt.Desktop;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileClass {
+
+		public String CurrentDir = System.getProperty("user.dir");
+		
+		public void createTextFile(String fileName , String path) {
+			
+				String fname= path + File.separator + fileName +".txt";
+			    File f = new File(path);
+			    File f1 = new File(fname);
+
+			    f.mkdirs();
+			    try {
+			        f1.createNewFile();
+			    } catch (IOException e) {}
+		}
+		
+		public void writeToTextFile(String fileName , String addedText , String path) {
+			
+			BufferedWriter bw = null;
+			FileWriter fw = null;
+			
+			//: Gets the path
+			String fname= path + File.separator + fileName +".txt";
+
+			try {
+					
+				//: Writes to the file
+				fw = new FileWriter(fname);
+				bw = new BufferedWriter(fw);
+				bw.write(addedText);
+
+			} catch (IOException e) {} finally {
+
+				try {
+					// closes the writers
+					if (bw != null)
+						bw.close();
+
+					if (fw != null)
+						fw.close();
+
+				} catch (IOException ex) {}
+
+			}	
+			
+		}
+		
+		public void writeToExistingFile(String fileName , String addedText , String path) {
+			
+			BufferedWriter bw = null;
+			FileWriter fw = null;
+			
+			//: Gets the path
+			String fname= path + File.separator + fileName +".txt";
+			
+			String existingText = readFromTextFile(fileName , path);
+
+			try {
+					
+				//: Writes to the file
+				fw = new FileWriter(fname);
+				bw = new BufferedWriter(fw);
+				bw.write(existingText + addedText);
+
+			} catch (IOException e) {} finally {
+
+				try {
+					// closes the writers
+					if (bw != null)
+						bw.close();
+
+					if (fw != null)
+						fw.close();
+
+				} catch (IOException ex) {}
+
+			}	
+			
+			
+		}
+		
+		public String readFromTextFile(String fileName , String path) {
+		
+			BufferedReader br = null;
+			FileReader fr = null;
+
+			//: Gets the path
+			String fname = path + File.separator + fileName +".txt";
+			
+			String output = null;
+			
+			try {
+		
+				fr = new FileReader(fname);
+				br = new BufferedReader(fr);
+
+				String sCurrentLine;
+				output = "";
+				while ((sCurrentLine = br.readLine()) != null) {
+					output += sCurrentLine;
+				}
+
+			} catch (IOException e) {} finally {
+
+				try {
+
+					if (br != null)
+						br.close();
+
+					if (fr != null)
+						fr.close();
+
+				} catch (IOException ex) {}
+
+			}
+			
+			return output;
+		}
+		
+		public void createFolder(String folderName , String path) {
+			try {
+				new File(path + "/" + folderName).mkdir();
+			} catch (Exception e) {}
+		}
+		
+		public void openExplorerWindow(String path) {
+			try {
+				Desktop.getDesktop().open(new File(path));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+}
--- src/main.java
+++ src/main.java
@@ -1,14 +1,35 @@
+import com.google.gson.Gson;
 
 public class main {
 
+	FileClass file = new FileClass();
+	Gson gson = new Gson();
+	
+	Library library = new Library();
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("hello again");
-		System.out.println("Just wanted to try this out n stuff");
 		
-		// hello
 		
-		//: yo
+		
 	}
 
+	public void saveLibrary() {
+	
+		String json = gson.toJson(library);
+		
+		file.createTextFile("Data base", file.CurrentDir);
+		
+		file.writeToExistingFile("Data base" ,json , file.CurrentDir);
+		
+	}
+	
+	public void loadLibrary() {
+		
+		String json = file.readFromTextFile( "Data base" , file.CurrentDir);
+		
+		library = gson.fromJson(json , Library.class);
+		
+	}
+	
+	
 }
