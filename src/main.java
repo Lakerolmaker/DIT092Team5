8da85a20d1f942413d137b099b7da541fc0f4917
jansson215 <jansson215@gmail.com>
2017-11-03 11:08:33 +0100

    Something here

--- src/main.java
+++ src/main.java
@@ -4,6 +4,7 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello again");
+		System.out.println("Just wanted to try this out n stuff");
 	}
 
 }
