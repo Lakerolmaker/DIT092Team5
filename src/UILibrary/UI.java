0fad05e17a2004fdb1f0fef187ad6428c288ffc2
Lakerolmaker <jacob.n.olsson@hotmail.com>
2017-11-15 09:46:31 +0100

    fixed issues

--- src/UILibrary/UI.java
+++ src/UILibrary/UI.java
@@ -1,38 +1,21 @@
 package UILibrary;
 
-import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.Dimension;
-import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.GraphicsDevice;
 import java.awt.GraphicsEnvironment;
-import java.awt.GridBagConstraints;
-import java.awt.LayoutManager;
-import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
-import java.sql.Date;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Calendar;
-import java.util.List;
 
-import javax.swing.BorderFactory;
-import javax.swing.DefaultComboBoxModel;
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JLayeredPane;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JProgressBar;
@@ -41,29 +24,11 @@ import javax.swing.JSlider;
 import javax.swing.JSpinner;
 import javax.swing.JTable;
 import javax.swing.JTextField;
-import javax.swing.ListSelectionModel;
 import javax.swing.ScrollPaneConstants;
-import javax.swing.SpinnerDateModel;
 import javax.swing.SpinnerListModel;
 import javax.swing.SpinnerModel;
-import javax.swing.SwingUtilities;
 import javax.swing.border.Border;
-import javax.swing.border.EtchedBorder;
-import javax.swing.event.ChangeListener;
-import javax.swing.event.ListSelectionListener;
-import javax.swing.table.TableModel;
 
-import com.sun.java.swing.plaf.gtk.GTKConstants.Orientation;
-import com.sun.prism.paint.Color;
-import com.sun.xml.internal.ws.api.config.management.policy.ManagementAssertion.Setting;
-import com.sun.xml.internal.ws.api.server.Container;
-
-import JackeLibrary.console;
-import UILibrary.*;
-import javafx.geometry.Insets;
-import javafx.scene.image.Image;
-
-import static JackeLibrary.Input.*;
 
 public class UI {
 	
@@ -546,8 +511,8 @@ public class UI {
 			selectedField.setBounds(selectedField.getX() + xIncrament, selectedField.getY() + yIncrament, selectedField.getWidth(), selectedField.getHeight());
 			selectedField.updateUI();
 			
-			console.log("X : " + selectedField.getX() + "\n");
-			console.log("Y : " + selectedField.getY() + "\n\n");
+			System.out.println("X : " + selectedField.getX() + "\n");
+			System.out.println("Y : " + selectedField.getY() + "\n\n");
 			
 		}else if(elementName == "button") {
 			JButton selectedButton = null;
@@ -559,8 +524,8 @@ public class UI {
 			selectedButton.setBounds(selectedButton.getX()  + xIncrament, selectedButton.getY() + yIncrament , selectedButton.getWidth() , selectedButton.getHeight() );
 			selectedButton.updateUI();
 			
-			console.log("X : " + selectedButton.getX() + "\n");
-			console.log("Y : " + selectedButton.getY() + "\n\n");
+			System.out.println("X : " + selectedButton.getX() + "\n");
+			System.out.println("Y : " + selectedButton.getY() + "\n\n");
 
 		}else if(elementName == "label") {
 			JLabel selectedLabel = null;
@@ -573,8 +538,8 @@ public class UI {
 			selectedLabel.getParent().setBounds(selectedLabel.getX()  + xIncrament, selectedLabel.getParent().getY() + yIncrament , selectedLabel.getParent().getWidth() , selectedLabel.getHeight() );
 			selectedLabel.updateUI();
 			
-			console.log("X : " + selectedLabel.getParent().getX() + "\n");
-			console.log("Y : " + selectedLabel.getParent().getY() + "\n\n");
+			System.out.println("X : " + selectedLabel.getParent().getX() + "\n");
+			System.out.println("Y : " + selectedLabel.getParent().getY() + "\n\n");
 
 		}else if(elementName == "progressBar") {
 			JProgressBar progressBar = null;
@@ -588,15 +553,15 @@ public class UI {
 			progressBar.setBounds(progressBar.getX()  , progressBar.getY()  , progressBar.getWidth() + xIncrament, progressBar.getHeight() + yIncrament);
 			progressBar.updateUI();
 			
-			console.log("X : " + progressBar.getX() + "\n");
-			console.log("Y : " + progressBar.getY() + "\n\n");
+			System.out.println("X : " + progressBar.getX() + "\n");
+			System.out.println("Y : " + progressBar.getY() + "\n\n");
 
 		}else if(elementName == "frame") {
 			
 			frame.setSize(frame.getWidth() + xIncrament, frame.getHeight() + yIncrament);
 			
-			console.log("X : " + frame.getWidth() + "\n");
-			console.log("Y : " + frame.getHeight() + "\n\n");
+			System.out.println("X : " + frame.getWidth() + "\n");
+			System.out.println("Y : " + frame.getHeight() + "\n\n");
 
 		}
 		
@@ -1181,11 +1146,13 @@ public class UI {
 		 
 	 }
 	
-	 public void getValues(String ID) {
+	 public GridReturn getValues(String ID) {
 		//int column = getter.getGridTable(ID).getSelectedColumn();
 		int[] row = getter.getGridTable(ID).getSelectedRows();
-		console.log(row);
-		//return new GridReturn(0 , row);
+		System.out.println(row);
+		
+		
+		return new GridReturn(0 , 0);
 	 }
 
 	};
