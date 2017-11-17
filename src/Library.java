09b8de99c008536be9b4ab52c7bfb854507ab066
Lakerolmaker <jacob.n.olsson@hotmail.com>
2017-11-17 17:54:46 +0100

    moved the save function to the library

--- src/Library.java
+++ src/Library.java
@@ -1,7 +1,28 @@
+import com.google.gson.Gson;
 
 public class Library {
-
 	
+	FileClass file = new FileClass();
+	Gson gson = new Gson();
+	
+
+	public void save() {
+		
+		String json = gson.toJson(main.library);
+		
+		file.createTextFile("Data base", file.CurrentDir);
+		
+		file.writeToExistingFile("Data base" ,json , file.CurrentDir);
+		
+	}
+	
+	public void load() {
+		
+		String json = file.readFromTextFile( "Data base" , file.CurrentDir);
+		
+		main.library = gson.fromJson(json , Library.class);
+		
+	}
 	
 	
 }
--- src/main.java
+++ src/main.java
@@ -2,32 +2,13 @@ import com.google.gson.Gson;
 
 public class main {
 
-	FileClass file = new FileClass();
-	Gson gson = new Gson();//this is a comment
-	
-	Library library = new Library();
+	public static Library library = new Library();
 	
 	public static void main(String[] args) {
 
 	}
 
-	public void saveLibrary() {
-	
-		String json = gson.toJson(library);
-		
-		file.createTextFile("Data base", file.CurrentDir);
-		
-		file.writeToExistingFile("Data base" ,json , file.CurrentDir);
-		
-	}
-	
-	public void loadLibrary() {
-		
-		String json = file.readFromTextFile( "Data base" , file.CurrentDir);
-		
-		library = gson.fromJson(json , Library.class);
-		
-	}
+
 	
 	
 }
