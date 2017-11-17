bfc712b39a50ccfc792eb88d228ec4378dabf8d8
TihanaC <tihanacausevic06@yahoo.se>
2017-11-17 15:29:18 +0100

    User class
    User class with all the attributes and methods (by Tihana)

---
+++ src/User.java
@@ -0,0 +1,72 @@
+
+	import java.util.HashMap;
+	import java.util.Date;
+
+	public class User {
+		private String name;
+		private int ssn;
+		private int userId;
+		private int libraryCardNum;
+		private double debt;
+		private int phoneNr;
+		private Object address;
+		
+		private HashMap<Book, Date> booksBorrowed = new HashMap<Book, Date>();
+		
+		User (String name, int userId, double debt, int ssn, int phoneNr, Object adress) {
+			this.name = name;
+			this.userId = userId;
+			this.libraryCardNum = this.userId;
+			this.debt = debt;
+			this.ssn = ssn;
+			this.phoneNr = phoneNr;
+			this.address = adress;
+		}
+		
+		public String getName() {
+			return this.name;
+		}
+		
+		public int getUserId() {
+			return this.userId;
+		}
+		
+		public double getDebt() {
+			return this.debt;
+		}
+		public int getSsn() {
+			return this.ssn;
+		}
+		public int getPhoneNr() {
+			return this.phoneNr;
+		}
+		public Object getAdress() {
+			return this.address;
+		}
+		
+		public void setName(String name) {
+			this.name = name;
+		}
+		public void setDebt(double debt) {
+			this.debt = debt;
+		}
+		public void setUserId(int userId) {
+			this.userId = userId;
+		}
+		public void setPhoneNr(int phoneNr) {
+			this.phoneNr = phoneNr;
+		}
+		public void setAddress(Object adress) {
+			this.address = address;
+		}
+		
+		public void borrowBook(Book book) {
+			Date date = new Date();
+			booksBorrowed.put(book, date);
+		}
+		
+		public void removeBorrowedBook(Book book) {
+			booksBorrowed.remove(book);
+		}
+
+}
