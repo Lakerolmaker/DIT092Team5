202ffe979b6aee0d18128d3525b6e49af6eb899b
tarikdurakovic <33163714+tarikdurakovic@users.noreply.github.com>
2017-11-21 18:40:52 +0100

    Adding some sorting methods
    created books arraylist as well

--- src/Library.java
+++ src/Library.java
@@ -1,9 +1,59 @@
 import com.google.gson.Gson;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Library {
 	
 	FileClass file = new FileClass();
 	Gson gson = new Gson();
+	
+	ArrayList<Book> books;
+
+	public Library() {
+
+		books = new ArrayList<Book>();
+
+	}
+	
+	public void sortBooksAuthorAZ() {
+
+		Collections.sort(books, Book.authorComparatorAZ);
+
+		for (Book str : books) {
+			System.out.println(str);
+		}
+
+	}
+
+	public void sortBooksAuthorZA() {
+
+		Collections.sort(books, Book.authorComparatorZA);
+
+		for (Book str : books) {
+			System.out.println(str);
+		}
+
+	}
+
+	public void sortBooksNameAZ() {
+
+		Collections.sort(books, Book.nameComparatorAZ);
+
+		for (Book str : books) {
+			System.out.println(str);
+		}
+
+	}
+
+	public void sortBooksNameZA() {
+
+		Collections.sort(books, Book.nameComparatorZA);
+
+		for (Book str : books) {
+			System.out.println(str);
+		}
+
+	}
 	
 
 	public void save() {
