d1131bb619a6ab84285541d425284f52e8d274ee
nadja555 <guskosina@student.gu.se>
2017-12-11 00:00:29 +0100

    still tried to get started after redesigning my gui

--- src/frontend/userListUI/UserListUI.java
+++ src/frontend/userListUI/UserListUI.java
@@ -1,109 +1,134 @@
 package frontend.userListUI;
 
 
-import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
-import frontend.*;
-import frontend.delayedBooksUI.*;
-import frontend.bookViewUI.BookViewUI;
-import frontend.booksUI.*;
-import frontend.emptyTemplateUI.EmptyTemplateUI;
-import frontend.homeUI.HomeUI;
-import frontend.registerUserUI.RegisterUserUI;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.VBox;
-import frontend.newBookUI.*;
-import program.Book;
-import userSearchUI.UserSearchUI;
+        import java.io.IOException;
+        import java.net.URL;
+        import java.util.ArrayList;
+        import java.util.HashMap;
+        import java.util.Map.Entry;
+        import java.util.ResourceBundle;
+
+        import frontend.booksUI.BooksUI;
+        import javafx.fxml.FXMLLoader;
+        import javafx.scene.Scene;
+        import javafx.scene.layout.HBox;
+        import javafx.scene.layout.VBox;
+        import javafx.scene.text.*;
+        import frontend.newBookUI.*;
+        import frontend.delayedBooksUI.*;
+        import frontend.homeUI.HomeUI;
+        import frontend.MainWindow;
+        import frontend.registerUserUI.*;
+        import frontend.userListUI.*;
+        import frontend.bookViewUI.*;
+        import javafx.beans.property.SimpleStringProperty;
+        import javafx.collections.FXCollections;
+        import javafx.collections.ObservableList;
+        import javafx.event.ActionEvent;
+        import javafx.fxml.FXML;
+        import javafx.fxml.Initializable;
+        import javafx.scene.control.Button;
+        import javafx.scene.control.CheckBox;
+        import javafx.scene.control.ContextMenu;
+        import javafx.scene.control.Label;
+        import javafx.scene.control.ListView;
+        import javafx.scene.control.MenuItem;
+        import javafx.scene.control.TableColumn;
+        import javafx.scene.control.TableView;
+        import javafx.scene.control.TextField;
+        import javafx.scene.control.cell.PropertyValueFactory;
+        import javafx.scene.input.MouseButton;
+        import javafx.scene.input.MouseEvent;
+        import program.*;
 
 
-/**
- * Empty template on how to initialize a 
- *
- */
 
-// NADJA is awesome"!
-public class UserListUI implements Initializable{
-	private static VBox root;
-	private static Scene emptyTemplate;
-	
-	@FXML private Label topMenu1; // Link to the fx:id in scenebuilder
-	@FXML private Button showNewBook;
-	
-	@Override
-	public void initialize(URL location, ResourceBundle resources) {
-		// TODO: Code goes here :)
-		// Example
-		//topMenu1.setText("Header Here");
-		
-		
-		
-	}
-	
-	public static void display() {
-		Class context = UserListUI.class;
-		try {
-			// This is the scene that is going to be shown inside the window ( Main window in this case )
-			VBox homeView = (VBox)FXMLLoader.load(context.getResource("UserListUI.fxml")); 
-			emptyTemplate = new Scene(homeView,1192,650);
-			emptyTemplate.getStylesheets().add(MainWindow.css);
-			
-
-			// Set the main window to show this scene
-			MainWindow.window.setScene(emptyTemplate);
-			MainWindow.window.show();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	
-	public void NewBookButtonClicked(){
-		NewBookUI.display();
-	}
+public class UserListUI implements Initializable {
+    //private static VBox userListView;
+    private static Scene userListScene;
 
 
-	
+    @FXML public TableView<User> tableUser;
+    @FXML public TableColumn<User, String> nameColumn;
+    @FXML public TableColumn<User, String> surnameColumn;
+    @FXML public Label menuHome;
+    @FXML public Label menuBooks;
+    @FXML public Label menuUsers;
+    @FXML public Label menuDelayed;
+    @FXML public Label menuRegisterUser;
+    @FXML public TextField searchFieldUser;
+    @FXML public Button BtnSearch;
+    @FXML public Label label1;
+    @FXML public Label label2;
+    @FXML public Button BtnShowAllUsers;
+    @FXML public Button BtnShowUsersWithDebts;
+    @FXML public Button BtnShowUsersWithBooks;
 
-	
-	
-	/******** File MENU ********/
-	public void newBook(){
-		NewBookUI.display();
-	}
-	public void quitMenuClick() {
-		MainWindow.closeProgram();
-	}
-	
-	/******** Main menu ********/
-	public void homeMenuAction(){
-		HomeUI.display();
-	}
-	public void booksMenuAction(){
-		BooksUI.display();
-	}
-	public void usersMenuAction() {
-		UserListUI.display();
-	}
-	public void openDelayedBooks() {
-		DelayedBook.display();
-	}	
-	public void openRegister() {
-		RegisterUserUI.display();
-	}
+    private static String searchFieldString = "";
 
-	/******** User List Buttons ******/
-	public void seachForUserAction(){
-		UserSearchUI.display();
-	}
-	
-	
+
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        searchFieldUser.setText(searchFieldString);
+
+        menuHome.setId("menuHome");
+        menuBooks.setId("menuBooks");
+        menuUsers.setId("menuBooks");
+        menuDelayed.setId("menuDelayed");
+
+
+
+
+    }
+
+    public static void display() {
+
+        try {
+            Class<UserListUI> context = UserListUI.class;
+            VBox userListView = (VBox)FXMLLoader.load(context.getResource("UserListFxml.fxml"));
+            userListScene = new Scene(userListView,1192,650);
+            userListScene.getStylesheets().add(MainWindow.css);
+
+            MainWindow.window.setScene(userListScene);
+            MainWindow.window.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+
+    /******** File MENU ********/
+    public void newBook(){
+        NewBookUI.display();
+    }
+    public void quitMenuClick() {
+        MainWindow.closeProgram();
+    }
+
+    /******** Main menu ********/
+    public void homeMenuAction(){
+        HomeUI.display();
+    }
+    public void booksMenuAction(){
+        BooksUI.display();
+    }
+    public void usersMenuAction() {
+        UserListUI.display();
+    }
+    public void goToBookView(Book book){
+        BookViewUI.display(book);
+    }
+    public void openDelayedBooks() {
+        DelayedBook.display();
+    }
+    public void openRegister() {
+        RegisterUserUI.display();
+    }
 
 }
