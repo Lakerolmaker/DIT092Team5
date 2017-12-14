160f98e3b35dfc77e8c5146944e85f4b46b7d0d6
nadja555 <guskosina@student.gu.se>
2017-12-14 17:40:19 +0100

    worked on userlistUI
    tableuser etc.

--- src/frontend/userListUI/UserListUI.java
+++ src/frontend/userListUI/UserListUI.java
@@ -9,8 +9,11 @@ package frontend.userListUI;
         import java.util.ResourceBundle;
 
         import frontend.booksUI.BooksUI;
+        import frontend.userprofileUI.UserProfileUI;
         import javafx.fxml.FXMLLoader;
         import javafx.scene.Scene;
+        import javafx.scene.image.Image;
+        import javafx.scene.image.ImageView;
         import javafx.scene.layout.HBox;
         import javafx.scene.layout.VBox;
         import javafx.scene.text.*;
@@ -45,43 +48,49 @@ package frontend.userListUI;
 
 public class UserListUI implements Initializable {
     //private static VBox userListView;
-    private static Scene userListScene;
 
 
     @FXML public TableView<User> tableUser;
+
     @FXML public TableColumn<User, String> nameColumn;
     @FXML public TableColumn<User, String> surnameColumn;
+    @FXML public TableColumn<User, String> idColumn;
+    @FXML public TableColumn<User, String> amountBooksColumn;
+    @FXML public TableColumn<User, String> amountDebtColumn;
+
     @FXML public Label menuHome;
     @FXML public Label menuBooks;
     @FXML public Label menuUsers;
     @FXML public Label menuDelayed;
     @FXML public Label menuRegisterUser;
+
     @FXML public TextField searchFieldUser;
-    @FXML public Button BtnSearch;
-    @FXML public Label label1;
+    @FXML public Button btnSearch;
+    @FXML public Label directoryOfUsers;
     @FXML public Label label2;
-    @FXML public Button BtnShowAllUsers;
-    @FXML public Button BtnShowUsersWithDebts;
-    @FXML public Button BtnShowUsersWithBooks;
+    @FXML public Button btnShowAllUsers;
+    @FXML public Button btnShowUsersWithDebts;
+    @FXML public Button btnShowUsersWithBooks;
+
+    @FXML public ImageView logoImage;
 
     private static String searchFieldString = "";
+    private static Scene userListScene;
+
 
 
 
 
     @Override
-    public void initialize(URL location, ResourceBundle resources) {
-
+    public void initialize(URL arg0, ResourceBundle arg1) {
+        Image logo = new Image("resources/logo.png");
+        logoImage.setImage(logo);
         searchFieldUser.setText(searchFieldString);
 
         menuHome.setId("menuHome");
         menuBooks.setId("menuBooks");
         menuUsers.setId("menuBooks");
         menuDelayed.setId("menuDelayed");
-
-
-
-
     }
 
     public static void display() {
@@ -99,8 +108,84 @@ public class UserListUI implements Initializable {
         }
     }
 
+    // Initialize table
+    public void initTable() {
+
+        // Name column
+        nameColumn = new TableColumn<>("Name");
+        nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+        nameColumn.setMaxWidth(8000);
+
+        // Surname column
+        surnameColumn = new TableColumn<>("Surname");
+        surnameColumn.setCellValueFactory(new PropertyValueFactory<>("surname"));
+        surnameColumn.setMaxWidth(9000);
+
+        // ID column
+
+        idColumn = new TableColumn<>("Id");
+        idColumn.setCellValueFactory(new PropertyValueFactory<>("id"));
+        idColumn.setMaxWidth(2500);
+        idColumn.setStyle("-fx-alignment: CENTER;");
+
+        //amount of borrowed books column
+        amountBooksColumn = new TableColumn<>("Books borrowed");
+        amountBooksColumn.setCellValueFactory(new PropertyValueFactory<>("books borrowed"));
+        amountBooksColumn.setStyle("-fx-alignment: CENTER;");
+
+        // amount debt column
+        amountDebtColumn = new TableColumn<>("Debt");
+        amountDebtColumn.setCellValueFactory(new PropertyValueFactory<>("debt"));
+        amountDebtColumn.setStyle("-fx-alignment: CENTER;");
 
 
+       tableUser.setItems(getUsers());
+        tableUser.getColumns().addAll(nameColumn, surnameColumn, idColumn, amountBooksColumn, amountDebtColumn);
+
+    }
+
+    // Return list of users
+    public ObservableList<User> getUsers() {
+        ObservableList<User> users = FXCollections.observableArrayList();
+        for (User user: MainWindow.lib.getUserList()) {
+            users.add(user);
+        }
+
+        return users;
+    }
+
+//TODO!!!
+    public ObservableList<User> getUsersWithBooks(){
+        ObservableList<User> usersWithBooks = FXCollections.observableArrayList();
+        for (User users: MainWindow.lib.getUserList()){
+            if(users.getBookList.size >0 ){
+
+            }
+        }
+
+    }
+
+    //throw away
+    // Return list of users
+    /*public void getUsers() {
+        ArrayList<User> listUsers = new ArrayList();
+        for (User user : MainWindow.lib.getUserList ()){
+
+            listUsers.add(user);
+        }
+    }
+*/
+    // table click functions
+    @FXML
+    public void clickItem(MouseEvent event) {
+
+        if (event.getClickCount() == 2) {
+            User selectedUser = tableUser.getSelectionModel().getSelectedItem(); // Retrieve selected cell
+            if (selectedUser != null) {
+                goToUserView(selectedUser);
+            }
+        }
+    }
 
 
     /******** File MENU ********/
@@ -129,6 +214,9 @@ public class UserListUI implements Initializable {
     }
     public void openRegister() {
         RegisterUserUI.display();
+    }
+    public void goToUserView(User user){
+        UserProfileUI.display(user);
     }
 
 }
