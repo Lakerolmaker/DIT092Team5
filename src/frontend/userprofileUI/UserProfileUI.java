a82341f07101a824c7ad9f4fc5c0878176976539
tarikdurakovic <33163714+tarikdurakovic@users.noreply.github.com>
2018-01-04 17:30:24 +0100

    Added more logo images

--- src/frontend/userprofileUI/UserProfileUI.java
+++ src/frontend/userprofileUI/UserProfileUI.java
@@ -42,6 +42,8 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
 import javafx.util.Callback;
@@ -69,9 +71,13 @@ public class UserProfileUI implements Initializable{
 		@FXML private Button edit;
 		@FXML private Button deleteUserbtn;
 		@FXML private Button btnHistory;
+		@FXML public ImageView logoImage;
 		
 		@Override
 		public void initialize(URL location, ResourceBundle resources) {
+			
+			Image logo = new Image("resources/logo.png");
+	        logoImage.setImage(logo);
 
             // loads the user information into the text fields
 			fName.setText(tmpuser.getFirstName());
